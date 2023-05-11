c4a934983af149a7fc1d58f3371ef022dfe11889
daisuke-coder <125184857+daisuke-coder@users.noreply.github.com>
2023-05-11 20:22:03 +0900

    Update create.blade.php

--- Laravel/resources/views/posts/create.blade.php
+++ Laravel/resources/views/posts/create.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+  <h2 class="page-header">新規投稿</h2>
+  {!!Form::open(['url'=>'post/create']) !!}
+  <div class="form-group">
+    {!! Form::input('text','newPost','null',['class'=>'form-control','placeholder'=>'投稿内容(120文字以内)']) !!}
+    @if(isset($e-message))
+    <p class="error">{{$e-message}}</p>
+    @endif
+    @if(isset($emptymessage))
+    <p class="error">{{$emptymessage}}</p>
+    @endif
 
+    {!! Form:hidden('newName',Auth::user()->name) !!}
+  </div>
+  <button class="post-button" type="submit">ツイート</button>
+  {!! Form::close() !!}
+</div>
 
 @endsection

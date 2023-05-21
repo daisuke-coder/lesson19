d6b71601ef87325042c98e20f39f6986185c349c
daisuke <daisuke.shn@gmail.com>
2023-05-21 22:34:22 +0900

    ログイン機能整備

--- Laravel/resources/views/layouts/app.blade.php
+++ Laravel/resources/views/layouts/app.blade.php
@@ -6,12 +6,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
   <title>webcritter</title>
 </head>
 <body>
   <header>
     <h1 class="page-header"><a href="/index" class="page-header">WEBCRITTER</a></h1>
+    <ul class="nav-menu">
+      @guest
+      <li class="nav-item"><a href="{{route('login')}}" class="nav-link">{{__('Login')}}</a></li>
+      <li class="nav-item"><a href="{{route('register')}}" class="nav-link">{{__('登録')}}</a></li>
+      @else
+      <li class="nav-item"><a href="/profile" class="nav-link">ユーザー名：{{ Auth::user()->name}}</a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+       <a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        {{ __('Logout') }}</a>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf </form>
+      </div>
+      </li class="nav-item">
+      @endguest
+    </ul>
   </header>
 
   @yield('content')
--- Laravel/storage/framework/views/25f42ef45b02dbcad349b38f53a2b2406e9d0c78.php
+++ Laravel/storage/framework/views/25f42ef45b02dbcad349b38f53a2b2406e9d0c78.php
@@ -6,12 +6,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
+  <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
 
   <title>webcritter</title>
 </head>
 <body>
   <header>
     <h1 class="page-header"><a href="/index" class="page-header">WEBCRITTER</a></h1>
+    <ul class="nav-menu">
+      <?php if(auth()->guard()->guest()): ?>
+      <li class="nav-item"><a href="<?php echo e(route('login')); ?>" class="nav-link"><?php echo e(__('Login')); ?></a></li>
+      <li class="nav-item"><a href="<?php echo e(route('register')); ?>" class="nav-link"><?php echo e(__('登録')); ?></a></li>
+      <?php else: ?>
+      <li class="nav-item"><a href="/profile" class="nav-link">ユーザー名：<?php echo e(Auth::user()->name); ?></a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+       <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"onclick="event.preventDefault();
+        document.getElementById('logout-form').submit();">
+        <?php echo e(__('Logout')); ?></a>
+
+        <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
+        <?php echo csrf_field(); ?> </form>
+      </div>
+      </li class="nav-item">
+      <?php endif; ?>
+    </ul>
   </header>
 
   <?php echo $__env->yieldContent('content'); ?>
